8ea7174dc807434b91158a212f4a4a9b12efa78e
Thái Nguyễn Việt Hùng <hungthai10a1@gmail.com>
2024-05-11 20:03:20 +0700

    feat: delete unnesscessary code

--- adapters/subcriber/notify_when_user_call.go
+++ adapters/subcriber/notify_when_user_call.go
@@ -2,8 +2,6 @@ package subcriber
 
 import (
 	"context"
-	"fmt"
-
 	pubsub "github.com/SeaCloudHub/notification-hub/adapters/realtime_pubsub"
 	"github.com/SeaCloudHub/notification-hub/adapters/skio"
 	noti "github.com/SeaCloudHub/notification-hub/domain/notification"
@@ -14,18 +12,11 @@ func NotifyWhenUserCall(rtEngine skio.RealtimeEngine, logger *zap.SugaredLogger,
 	return consumerJob{
 		Title: "Notification for user",
 		Hld: func(ctx context.Context, message *pubsub.Message) error {
-			fmt.Printf("day ne: %v\n", message.Data())
 			setNotifications := message.Data().(noti.SetOfNotifications)
-			fmt.Printf("Set of notifications: %v\n", setNotifications)
 			for _, notification := range setNotifications.Noitications {
-				fmt.Print("notication tung cai", notification)
-				fmt.Printf("content day ne: %v\n", notification.Content)
 				err := rtEngine.EmitToUser(notification.To, "notification", notification.Content)
-				fmt.Printf("content day ne da emit: %v\n", notification.Content)
-				fmt.Printf("loi ne: %v\n", err)
 
 				if err != nil {
-					// fmt.Printf("loi content day ne: %v\n", notification.Content)
 					if err := store.UpdateStatusByUid(ctx, notification.Uid, noti.StatusFailure); err != nil {
 						logger.Errorf("Error updating notification status failure: %v", err)
 					}
